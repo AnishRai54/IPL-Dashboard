8bee0175a60272c9a6e94bd882988b3d7151927a
AnishRai54 <anishkumar141212@gmail.com>
2026-03-05 16:44:49 +0530

    more function added

--- dbhelper.py
+++ dbhelper.py
@@ -30,7 +30,10 @@ import pandas as pd
 class DB:
 
     def __init__(self):
+        
         password = os.getenv("DB_Password")
+
+        
 
         try:
             self.conn = mysql.connector.connect(
@@ -38,14 +41,16 @@ class DB:
                 user="root",
                 password=password,
                 port=3306,
-                database="IPL"
+                database="ipl",
+                auth_plugin="mysql_native_password"
             )
 
             self.mycursor = self.conn.cursor()
-            print("Connection Establised")
+            print("✅ Connection Established")
 
         except mysql.connector.Error as e:
-            print(" Connection Error", e)
+            print("❌ Connection Error:", e)
+            raise e
 
     def fetch_Season(self):
         self.mycursor.execute(""" select distinct(year) from ipl_data;""")
@@ -144,9 +149,76 @@ class DB:
         data=self.mycursor.fetchall()
 
         return data
+    
+    def fetch_team_run(self,team):
+        query="""SELECT batting_team AS team, SUM(runs_total) AS total_runs
+                  FROM ipl_data
+                  GROUP BY batting_team 
+                  having team =%s ;"""
+        self.mycursor.execute(query,(team,))
+        data= self.mycursor.fetchall()
+        return data[0][1]
+    
+    
+    def fetch_total_matches(self,team):
+        query=""" SELECT team, COUNT(DISTINCT match_id) AS total_matches
+              FROM
+              (
+                  SELECT match_id, batting_team AS team FROM ipl_data
+                  UNION ALL
+                  SELECT match_id, bowling_team AS team FROM ipl_data
+              ) t
+              where team =%s
+              GROUP BY team
+              """
+        self.mycursor.execute(query,(team,))
+        data=self.mycursor.fetchone()[1]
+        return data
+    
+    def fetch_total_wicket(self,team):
+        query=""" select bowling_team,count(*) as Total_wicket from ipl_data 
+                    where wicket_kind is not null 
+                    and bowling_team=%s
+                    group by bowling_team  """
+        self.mycursor.execute(query,(team,))
+        data= self.mycursor.fetchone()[1]
+        return data
+    
+    def fetch_Season_runs(self,team,year):
+        query=""" select batting_team,sum(runs_total) as Total_Runs from ipl_data
+          where year=%s and batting_team=%s
+         group by batting_team"""
+         
+        self.mycursor.execute(query,(year,team))
+        data=self.mycursor.fetchone()[1]
+        
+        return data
+    
+    def fetch_season_wickets(self, team, year):
 
+        query = """
+        SELECT COUNT(*)
+        FROM ipl_data
+        WHERE wicket_kind IS NOT NULL
+        AND year=%s
+        AND bowling_team=%s
+        """
+
+        self.mycursor.execute(query,(year,team))
+        result = self.mycursor.fetchone()
+
+        return result[0]
+        
+
+    
+    
 db=DB()
-a=db.fetch_Point_table(2008)
+team="Chennai Super Kings"
+d=db.fetch_Season_runs(team,2009)
+print(d)
+
+
+
 
 
 
